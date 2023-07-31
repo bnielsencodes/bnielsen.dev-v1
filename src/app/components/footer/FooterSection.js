71ba6dbd046e26cb19cbe4c479122308fb155c8b
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-07-30 21:40:30 -0700

    Create FooterSection.js

---
+++ src/app/components/footer/FooterSection.js
@@ -0,0 +1,11 @@
+export default function FooterSection(props) {
+  return (
+    <>
+      <section id={styles["footer-section"]}>
+        <div className={styles.container}>
+          <p className={styles.signature}>Brandon Nielsen - 2023</p>
+        </div>
+      </section>
+    </>
+  );
+}
