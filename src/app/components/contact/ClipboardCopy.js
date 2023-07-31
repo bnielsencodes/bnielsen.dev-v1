54c9a8242a9ac707c090009ee77d8026f5381ef6
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-07-30 21:08:15 -0700

    Import and add styling

--- src/app/components/contact/ClipboardCopy.js
+++ src/app/components/contact/ClipboardCopy.js
@@ -1,5 +1,7 @@
 "use client";
 import { useState } from "react";
+import styles from "./ClipboardCopy.module.scss";
+
 export default function ClipboardCopy() {
   const [isCopied, setIsCopied] = useState(false);
 
