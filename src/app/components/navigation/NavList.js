2861cc4a8c1fa8961c217adc6552ef369f4c5277
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-07-30 20:03:51 -0700

    Create NavList.js

---
+++ src/app/components/navigation/NavList.js
@@ -0,0 +1,44 @@
+export default function NavList(props) {
+  return (
+    <>
+      <ul className={`${styles["nav-list"]} hidden--sm`}>
+        <li className={styles.item}>
+          <a
+            className={`${styles.link} ${styles["link--home"]} ${styles.underlineAnimation}`}
+            href={props.homePath}
+            aria-label="home"
+          >
+            Home
+          </a>
+        </li>
+        <li className={styles.item}>
+          <a
+            className={`${styles.link} ${styles.underlineAnimation}`}
+            href={props.aboutPath}
+            aria-label="about"
+          >
+            About
+          </a>
+        </li>
+        <li className={styles.item}>
+          <a
+            className={`${styles.link} ${styles.underlineAnimation}`}
+            href={props.projectsPath}
+            aria-label="projects"
+          >
+            Projects
+          </a>
+        </li>
+        <li className={styles.item}>
+          <a
+            className={`${styles.link} ${styles.underlineAnimation}`}
+            href={props.contactPath}
+            aria-label="contacts"
+          >
+            Contact
+          </a>
+        </li>
+      </ul>
+    </>
+  );
+}
