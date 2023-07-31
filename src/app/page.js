bdaa0f5ae8e120c93f111dfb2fe418fd6b95f017
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-07-30 20:01:36 -0700

    Import and add Header component

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,10 @@
+import Header from "@/app/components/header/Header";
 
 export default function Home() {
   return (
     <>
       <main className="main">
+        <Header />
         />
       </main>
     </>
