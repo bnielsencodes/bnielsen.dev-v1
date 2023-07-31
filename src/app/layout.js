ed0385dbb1908e031e7fa2038c4dde1d9bcaf251
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-07-30 19:16:00 -0700

    Add smooth scroll behavior

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,3 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -10,8 +6,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" style={{ scrollBehavior: "smooth" }}>
+      <body
+      // className={inter.className}
+      >
+        {children}
+      </body>
     </html>
-  )
+  );
 }
