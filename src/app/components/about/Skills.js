98a25556910087b26dc5824620baa78e3576f725
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-07-30 20:22:09 -0700

    Create Skills.js

---
+++ src/app/components/about/Skills.js
@@ -0,0 +1,11 @@
+export default function Skills() {
+  return (
+    <>
+        <aside className={styles.skills}>
+          <h4 className={styles.heading}>Top Skills</h4>
+          <div className={styles.container}>
+          </div>
+        </aside>
+    </>
+  );
+}
