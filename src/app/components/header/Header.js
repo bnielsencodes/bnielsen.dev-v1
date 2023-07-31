c78406ac716f2f5704a70a7e8077429b70359be1
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-07-30 19:39:21 -0700

    Create Header.js

---
+++ src/app/components/header/Header.js
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <>
+      <header id="header" className={styles.header}>
+        <div className={styles.container}>
+        </div>
+      </header>
+    </>
+  );
+}
