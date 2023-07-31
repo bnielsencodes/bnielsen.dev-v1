7804a0a01e956affd8542c659c776745e0403770
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-07-30 19:40:13 -0700

    Create HeaderCTABtn.js

---
+++ src/app/components/header/HeaderCTABtn.js
@@ -0,0 +1,13 @@
+export default function HeaderCTABtn() {
+  return (
+    <>
+      <div className={styles.container}>
+        <a className={styles.link} href="#top-divider" aria-label="view work">
+          View my work
+          <span className={styles.span}>
+          </span>
+        </a>
+      </div>
+    </>
+  );
+}
