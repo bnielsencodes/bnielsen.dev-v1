ae23b713e9e8f9d744a2d24a5e8f0a38ab9df04f
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-07-30 19:39:50 -0700

    Create HeaderIntro.js

---
+++ src/app/components/header/HeaderIntro.js
@@ -0,0 +1,14 @@
+export default function HeaderIntro() {
+  return (
+    <>
+      <div className={styles.container}>
+        <h2 className={styles.heading}>
+          Hi, I&apos;m
+          <span className={styles.span}> Brandon</span>
+          <span className={styles.span}> Nielsen</span>,
+        </h2>
+        <h2 className={styles.subheading}>a front-end web developer.</h2>
+      </div>
+    </>
+  );
+}
