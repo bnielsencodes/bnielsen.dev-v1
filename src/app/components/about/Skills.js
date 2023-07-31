6d6deb10d1b3d72888a4397463ee424b95504289
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-07-30 20:23:24 -0700

    Import Skill component and skills data

--- src/app/components/about/Skills.js
+++ src/app/components/about/Skills.js
@@ -1,5 +1,7 @@
 "use client";
 import { motion } from "framer-motion";
+import { skills } from "../../../data";
+import Skill from "@/app/components/about/Skill";
 export default function Skills() {
   return (
     <>
