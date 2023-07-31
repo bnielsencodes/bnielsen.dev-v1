5481328874c901d97de9b5fc22fc4e5cda69590b
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-07-30 20:22:46 -0700

    Import and add framer motion wrapper

--- src/app/components/about/Skills.js
+++ src/app/components/about/Skills.js
@@ -1,11 +1,24 @@
+"use client";
+import { motion } from "framer-motion";
 export default function Skills() {
   return (
     <>
+      <motion.div
+        className={styles.motion}
+        initial={{ opacity: 0, translateX: "25rem" }}
+        whileInView={{ opacity: 1, translateX: "0" }}
+        transition={{
+          duration: 1.4,
+          ease: [0.21, 1.11, 0.81, 0.99],
+        }}
+        viewport={{ once: true }}
+      >
         <aside className={styles.skills}>
           <h4 className={styles.heading}>Top Skills</h4>
           <div className={styles.container}>
           </div>
         </aside>
+      </motion.div>
     </>
   );
 }
