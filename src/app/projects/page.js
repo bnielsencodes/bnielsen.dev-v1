fe7ff3dab61bcd1cb33a03f542440fc86e76d327
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-07-30 21:46:11 -0700

    Import and add Navigation component

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,7 +1,14 @@
+import Navigation from "@/app/components/navigation/Navigation";
 export default function Projects() {
   return (
     <>
       <main id="main">
+        <Navigation
+          homePath="/#header"
+          aboutPath="/#about-section"
+          projectsPath="#main"
+          contactPath="#contact-section-anchor"
+        />
       </main>
     </>
   );
