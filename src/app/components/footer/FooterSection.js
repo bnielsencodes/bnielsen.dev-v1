fa7b610f8e7a1fad537ec38731b909221f9a6f8f
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-07-30 21:43:20 -0700

    Import and add ReturnHomeBtn component

--- src/app/components/footer/FooterSection.js
+++ src/app/components/footer/FooterSection.js
@@ -1,3 +1,4 @@
+import ReturnHomeBtn from "./ReturnHomeBtn";
 import FooterSocials from "@/app/components/footer/FooterSocials";
 import styles from "./FooterSection.module.scss";
 
@@ -5,6 +6,7 @@ export default function FooterSection(props) {
   return (
     <>
       <section id={styles["footer-section"]}>
+        <ReturnHomeBtn returnHomePath={props.returnHomePath} />
         <div className={styles.container}>
           <FooterSocials />
           <p className={styles.signature}>Brandon Nielsen - 2023</p>
