c460fa8ea6a27288ac876d328d660c14a2d623dc
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-07-30 20:55:38 -0700

    Create ProjectBtns.js

---
+++ src/app/components/projects/ProjectBtns.js
@@ -0,0 +1,22 @@
+export default function ProjectBtns(props) {
+  return (
+    <>
+      <div className={styles.buttons}>
+        <a
+          className={styles.liveSite}
+          href={props.project.liveSite}
+          target="_blank"
+        >
+          Live Site
+        </a>
+        <a
+          className={styles.sourceCode}
+          href={props.project.sourceCode}
+          target="_blank"
+        >
+          Source Code
+        </a>
+      </div>
+    </>
+  );
+}
