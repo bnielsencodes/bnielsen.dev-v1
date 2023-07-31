b28aad8d14823ce6720f0889360ea9b3ee61d01e
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-07-30 20:46:36 -0700

    Import and map through projects data

--- src/app/components/projects/ProjectsSection.js
+++ src/app/components/projects/ProjectsSection.js
@@ -1,3 +1,10 @@
+import { projects } from "../../../data";
+import FeaturedProject from "@/app/components/projects/FeaturedProject";
+
+const featuredProjects = projects.slice(0, 4).map((project) => {
+  return <FeaturedProject key={project.id} project={project} />;
+});
+
 export default function ProjectsSection() {
   return (
     <>
