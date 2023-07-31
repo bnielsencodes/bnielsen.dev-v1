93b6394fa388e3124dab67b9a2cb2603b6303890
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-07-30 20:43:39 -0700

    Create ProjectsSection.js

---
+++ src/app/components/projects/ProjectsSection.js
@@ -0,0 +1,11 @@
+export default function ProjectsSection() {
+  return (
+    <>
+      <section id={styles["projects-section"]}>
+        <h2 className={styles.heading}>
+          <span className={styles.span}>Projects</span>
+        </h2>
+      </section>
+    </>
+  );
+}
