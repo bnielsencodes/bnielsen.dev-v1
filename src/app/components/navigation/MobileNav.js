614c9c2c533dab3de5aad1e42f30cc02ca819b27
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-07-30 20:05:21 -0700

    Create MobileNav.js

---
+++ src/app/components/navigation/MobileNav.js
@@ -0,0 +1,44 @@
+export default function MobileNav(props) {
+  return (
+    <>
+      <div className={`${styles["mobile-nav"]} `}>
+        <div className={styles.container}>
+          <ul className={styles.list}>
+            <li>
+              <a className={styles.link} href="#header" aria-label="home">
+                Home
+              </a>
+            </li>
+            <li>
+              <a
+                className={styles.link}
+                href="#about-section"
+                aria-label="about"
+              >
+                About
+              </a>
+            </li>
+            <li>
+              <a
+                className={styles.link}
+                href="#top-divider"
+                aria-label="projects"
+              >
+                Projects
+              </a>
+            </li>
+            <li>
+              <a
+                className={styles.link}
+                href="#bottom-divider"
+                aria-label="contact"
+              >
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
