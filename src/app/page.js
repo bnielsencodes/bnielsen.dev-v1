6f074c9f919aa4c51e8fcc2a114ed0de82590551
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-07-30 20:26:05 -0700

    Import and add DividerTop and DividerBottom components

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,8 @@
 import Header from "@/app/components/header/Header";
 import Navigation from "@/app/components/navigation/Navigation";
 import AboutSection from "@/app/components/about/AboutSection";
+import DividerTop from "@/app/components/projects/DividerTop";
+import DividerBottom from "@/app/components/projects/DividerBottom";
 
 export default function Home() {
   return (
@@ -14,6 +16,8 @@ export default function Home() {
           contactPath="#bottom-divider"
         />
         <AboutSection />
+        <DividerTop id="top-divider" />
+        <DividerBottom id="bottom-divider" />
       </main>
     </>
   );
