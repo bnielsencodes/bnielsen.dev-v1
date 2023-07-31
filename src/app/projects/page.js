338d3895ec22e156555bf8083e7a1c6fc6718b72
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-07-30 21:48:38 -0700

    Import and add Project components

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,5 +1,6 @@
 import { projects } from "../../data";
 import Navigation from "@/app/components/navigation/Navigation";
+import Project from "@/app/components/projects/Project";
 import ContactSection from "@/app/components/contact/ContactSection";
 import FooterSection from "@/app/components/footer/FooterSection";
 
@@ -19,6 +20,12 @@ export default function Projects() {
         />
         {/* <!-- PAGE TOP DIVIDER --> */}
         <div className={styles["top-divider"]}></div>
+        <section className={styles.projects}>
+          <h2 className={styles.heading}>
+            <span className={styles.span}>Projects</span>
+          </h2>
+          <div className={styles.list}>{projectsList}</div>
+        </section>
         <div id="contact-section-anchor"></div>
         <ContactSection projectPage={true} />
         <FooterSection returnHomePath="#main" />
