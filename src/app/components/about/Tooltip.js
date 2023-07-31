90332924231b370c6fcf4e8b3a21a15408d4829c
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-07-30 20:17:41 -0700

    Create Tooltip.js

---
+++ src/app/components/about/Tooltip.js
@@ -0,0 +1,16 @@
+export default function Tooltip(props) {
+  return (
+    <>
+      <div
+        id={styles.tooltip}
+        className={styles[`tooltip--${props.name}`]}
+        style={{ display: props.showTooltip ? "inline-block" : "none" }}
+      >
+        <div className={`${styles.arrow} ${styles[`arrow--${props.name}`]}`} />
+        <div className={`${styles["label"]} ${styles[`label--${props.name}`]}`}>
+          {props.tech}
+        </div>
+      </div>
+    </>
+  );
+}
