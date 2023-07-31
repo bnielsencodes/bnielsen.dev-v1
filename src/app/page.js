eed72692e66e96b86ed6575fd2cb5d3d1d89facd
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-07-30 20:08:54 -0700

    Import and add Navigation component

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,16 @@
 import Header from "@/app/components/header/Header";
+import Navigation from "@/app/components/navigation/Navigation";
 
 export default function Home() {
   return (
     <>
       <main className="main">
         <Header />
+        <Navigation
+          homePath="#header"
+          aboutPath="#about-section"
+          projectsPath="#top-divider"
+          contactPath="#bottom-divider"
         />
       </main>
     </>
