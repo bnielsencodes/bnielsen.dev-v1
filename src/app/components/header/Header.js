530442eeb3363224f4116e22880ebd31b8b69cf6
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-07-30 19:41:36 -0700

    Import and add HeaderIntro component

--- src/app/components/header/Header.js
+++ src/app/components/header/Header.js
@@ -1,8 +1,11 @@
+import HeaderIntro from "@/app/components/header/HeaderIntro";
+
 export default function Header() {
   return (
     <>
       <header id="header" className={styles.header}>
         <div className={styles.container}>
+          <HeaderIntro />
         </div>
       </header>
     </>
