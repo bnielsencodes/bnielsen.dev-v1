01794311df771e8b24cd1d9faf2f3c997aedfc4c
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-07-30 21:07:19 -0700

    Create functions
    copyTextToClipboard, and handleCopyClick

--- src/app/components/contact/ClipboardCopy.js
+++ src/app/components/contact/ClipboardCopy.js
@@ -3,6 +3,32 @@ import { useState } from "react";
 export default function ClipboardCopy() {
   const [isCopied, setIsCopied] = useState(false);
 
+  // This is the function we wrote earlier
+  async function copyTextToClipboard(text) {
+    if ("clipboard" in navigator) {
+      return await navigator.clipboard.writeText(text);
+    } else {
+      return document.execCommand("copy", true, text);
+    }
+  }
+
+  // onClick handler function for the copy button
+  const handleCopyClick = (e) => {
+    e.preventDefault();
+    // Asynchronously call copyTextToClipboard
+    copyTextToClipboard("brandon@bnielsen.dev")
+      .then(() => {
+        // If successful, update the isCopied state value
+        setIsCopied(true);
+        setTimeout(() => {
+          setIsCopied(false);
+        }, 1500);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <>
       <button
